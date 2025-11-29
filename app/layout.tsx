f307a2601dfd2cd791b2f71a71f7bfec355000db
Jakub <jakubtomczykdev@gmail.com>
2025-11-29 13:34:38 +0100

    Business image fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export const metadata: Metadata = {
     locale: "pl_PL",
     images: [
       {
-        url: "/business.png",
+        url: "/logos/business.png",
         width: 1200,
         height: 630,
         alt: "Vexel Studio – Nowoczesne strony internetowe",
